7f77c4df25fc19ee9e043a6db1235d6b11c36d7f
sairam <sairam@AIML.SAIRAM.EDU.IN>
2025-10-10 11:52:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let wordList = ["apple", "ball", "cat", "dog", "fish", "chair", "table", "phone", "book", "mouse", "school", "tree", "flower", "water", "pizza", "orange", "bread", "shirt", "light", "music", "smile", "happy", "dance", "sleep", "dream", "movie", "beach", "cloud", "rain", "game",
+    "computer", "bicycle", "camera", "window", "teacher", "jungle", "planet", "travel", "mirror", "desert", "candle", "pocket", "energy", "family", "dragon", "island", "moment", "castle", "bridge", "kitchen", "rocket", "forest", "diamond", "galaxy", "crystal", "library", "monster", "thunder", "secret", "courage", "fortune", "kingdom", "leopard", "journey", "science", "compass", "justice", "temple", "engine", "volcano"
+    ]
+
+let index = Math.floor(Math.random()*30);
+let randomWord = wordList[index];
+let wordLen = randomWord.length;
+
+for(let i=0; i<wordLen; i++){
+    let newDiv = document.createElement('div');
+
+    let divClass = `box${i}`;
+    newDiv.classList.add(divClass);
+    newDiv.classList.add("box");
+
+    const container = document.querySelector(".container");
+    container.appendChild(newDiv);
+
+    let newPara = document.createElement('p');
+    // newPara.textContent = randomWord[i];
+
+    let paraClass = `p${i}`;
+    newPara.classList.add(paraClass);
+    newPara.classList.add('p');
+
+    const innerContainer = document.querySelector(".box" + i);
+    innerContainer.appendChild(newPara);
+}
+
+document.addEventListener("keypress", function(event){
+    let alpha = event.key;
+    let val = false;
+    
+    for (let i = 0; i < wordLen; i++) {
+        if (alpha == randomWord[i]){
+            const pAlpha = document.querySelector(`.p${i}`);
+            console.log(pAlpha)
+            
+            pAlpha.textContent = alpha;
+        }
+        
+    }
+
+});
